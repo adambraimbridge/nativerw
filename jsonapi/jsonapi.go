d9e7e77a9515583f162f698f74dc20da61348281
Martin Garton <martin.garton@ft.com>
2015-03-27 17:05:03 +0000

    initial commit

---
+++ jsonapi/jsonapi.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"net/http"
+	"os"
+	"reflect"
+	"time"
+)
+
+type MgoApi struct {
+	dbName         string
+	session        *mgo.Session
+	resourceIdName string
+	beforeWrite    propertyConverter
+	afterRead      propertyConverter
+}
+
+func NewMgoApi(dbName string, resourceIdName string, beforeWrite, afterRead propertyConverter) (*MgoApi, error) {
+	s, err := mgo.DialWithTimeout("localhost", time.Duration(3*time.Second))
+	if err != nil {
+		return nil, err
+	}
+	s.SetMode(mgo.Monotonic, true)
+
+	return &MgoApi{dbName, s, resourceIdName, beforeWrite, afterRead}, nil
+}
+
+func (ma *MgoApi) Write(collection string, resource map[string]interface{}) error {
+	coll := ma.session.DB(ma.dbName).C(collection)
+
+	ma.mongoizeAll(resource)
+
+	_, err := coll.Upsert(bson.D{{ma.resourceIdName, resource[ma.resourceIdName]}}, resource)
+
+	return err
+}
+
+func (ma *MgoApi) Read(collection string, resourceId string) (bool, interface{}) {
+	coll := ma.session.DB(ma.dbName).C(collection)
+
+	// convert resource id to mgo friendly form if needed
+	props := make(map[string]interface{})
+	props[ma.resourceIdName] = resourceId
+	ma.mongoizeAll(props)
+	mongoResourceId := props[ma.resourceIdName]
+
+	var resource map[string]interface{}
+	if err := coll.Find(bson.M{ma.resourceIdName: mongoResourceId}).One(&resource); err != nil {
+		if err == mgo.ErrNotFound {
+			return false, nil
+		}
+		panic(err)
+	}
+
+	ma.unmongoizeAll(resource)
+
+	return true, resource
+}
+
+func (ma *MgoApi) mongoizeAll(resource map[string]interface{}) {
+	for k, v := range resource {
+		if reflect.ValueOf(v).Type() == mapStrIfType {
+			ma.mongoizeAll(v.(map[string]interface{}))
+		} else {
+			pm := simplePropertyModifier{resource, k}
+			ma.beforeWrite(pm, resource, k, v)
+		}
+	}
+}
+
+var mapStrIfType = reflect.ValueOf(make(map[string]interface{})).Type()
+
+func (ma *MgoApi) unmongoizeAll(resource map[string]interface{}) {
+	for k, v := range resource {
+		if reflect.ValueOf(v).Type() == mapStrIfType {
+			ma.unmongoizeAll(v.(map[string]interface{}))
+		} else {
+			pm := simplePropertyModifier{resource, k}
+			ma.afterRead(pm, resource, k, v)
+		}
+	}
+}
+
+func (ma *MgoApi) readHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	resourceId := vars["resource"]
+	collection := vars["collection"]
+
+	found, resource := ma.Read(collection, resourceId)
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("resource with id %s was not found\n", resourceId)))
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.Encode(resource)
+}
+
+func (ma *MgoApi) writeHandler(w http.ResponseWriter, r *http.Request) {
+	collectionId := mux.Vars(r)["collection"]
+	resourceId := mux.Vars(r)["resource"]
+
+	dec := json.NewDecoder(r.Body)
+	var resource map[string]interface{}
+
+	if err := dec.Decode(&resource); err != nil {
+		http.Error(w, fmt.Sprintf("json decode failed: %v", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	if plResourceId := resource[ma.resourceIdName]; plResourceId != resourceId {
+		http.Error(w, "given resource id does not match payload", http.StatusBadRequest)
+		return
+	}
+
+	if err := ma.Write(collectionId, resource); err != nil {
+		http.Error(w, fmt.Sprintf("write failed:\n%v\n", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	m := mux.NewRouter()
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stdout, m))
+
+	ma, err := NewMgoApi("testdb", "uuid",
+		compositePropertyConverter{[]propertyConverter{UUIDToBson, DateToBson}}.convert,
+		compositePropertyConverter{[]propertyConverter{UUIDFromBson, DateFromBson, MongoIdRemover, ApiUrlInserter}}.convert,
+	)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err.Error())
+		return
+	}
+
+	m.HandleFunc("/{collection}/{resource}", ma.readHandler).Methods("GET")
+	m.HandleFunc("/{collection}/{resource}", ma.writeHandler).Methods("PUT")
+
+	http.ListenAndServe(":8082", nil)
+}
---
+++ jsonapi/property_convert.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"code.google.com/p/go-uuid/uuid"
+	"fmt"
+	"labix.org/v2/mgo/bson"
+	"strings"
+	"time"
+)
+
+type PropertyModifier interface {
+	Remove()
+	Update(interface{})
+	Put(string, interface{})
+	Name() string
+	Value() interface{}
+}
+
+type simplePropertyModifier struct {
+	values map[string]interface{}
+	name   string
+}
+
+func (spm simplePropertyModifier) Remove() {
+	delete(spm.values, spm.name)
+}
+
+func (spm simplePropertyModifier) Update(newValue interface{}) {
+	spm.values[spm.name] = newValue
+	//fmt.Printf("after update for %v the values are %v\n", spm.name, spm.values)
+}
+
+func (spm simplePropertyModifier) Put(name string, value interface{}) {
+	spm.values[name] = value
+	//fmt.Printf("after put for %v of %v=%v the values are %v\n", spm.name, name, value, spm.values)
+}
+
+func (spm simplePropertyModifier) Name() string {
+	return spm.name
+}
+
+func (spm simplePropertyModifier) Value() interface{} {
+	return spm.values[spm.name]
+}
+
+type propertyConverter func(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (stop bool)
+
+type compositePropertyConverter struct {
+	converters []propertyConverter
+}
+
+func (cpc compositePropertyConverter) convert(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (stop bool) {
+	for _, c := range cpc.converters {
+		stop := c(pm, context, name, pm.Value())
+		if stop {
+			return true
+		}
+	}
+	return false
+}
+
+func UUIDToBson(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (stop bool) {
+	if name == "uuid" {
+		bsonUuid := bson.Binary{Kind: 0x04, Data: []byte(uuid.Parse(value.(string)))}
+		//TODO: check it parsed ok. This might not really be a uuid
+		pm.Update(bsonUuid)
+	}
+	return false
+}
+
+func UUIDFromBson(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (stop bool) {
+	if name == "uuid" {
+		if bin, ok := value.(bson.Binary); ok && bin.Kind == 0x04 {
+			pm.Update(uuid.UUID(bin.Data).String())
+		}
+	}
+	return false
+}
+
+func DateToBson(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (changed bool) {
+	if strings.HasSuffix(name, "Date") {
+		if s, ok := value.(string); ok {
+			t, err := time.Parse(time.RFC3339, s)
+			if err == nil {
+				pm.Update(t)
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func DateFromBson(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (changed bool) {
+	if strings.HasSuffix(name, "Date") {
+		d, ok := value.(time.Time)
+		if ok {
+			pm.Update(d.Format(time.RFC3339))
+			return true
+		}
+	}
+	return false
+}
+
+func MongoIdRemover(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (changed bool) {
+	if name == "_id" {
+		pm.Remove()
+		return true
+	}
+	return false
+}
+
+func ApiUrlInserter(pm PropertyModifier, context map[string]interface{}, name string, value interface{}) (changed bool) {
+	if name == "uuid" {
+		pm.Put("apiUrl", fmt.Sprintf("http://localhost:8082/%s/%s", "content" /*FIXME*/, value))
+	}
+	return false
+}
