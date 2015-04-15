7e0c7b5811fd188a90eda3d82dd479a6848331cd
Hunor Kovács <hunor.kovacs@ft.com>
2015-04-15 18:10:27 +0300

    type fixed

--- nativerw_test.go
+++ nativerw_test.go
@@ -20,7 +20,7 @@ func TestWrapResource(t *testing.T) {
 			"application/json",
 			map[string]interface{} {
 				"uuid": "9694733e-163a-4393-801f-000ab7de5041",
-				"content": {
+				"content": map[string]interface{} {
 					"title": "Title", 
 					"body": "This is a body.",
 					"brands" : []string {"Lex", "Markets"},
