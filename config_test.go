55d1a7ca1ab4316a6bee3398083350030bbf130e
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-04-16 18:55:58 +0300

    Removed dummy test.

--- config_test.go
+++ config_test.go
@@ -5,28 +5,6 @@ import (
 	"strings"
 )
 
-func TestReadConfigFromReader(t *testing.T) {
-
-	var tests = [] struct {
-		json, wantDbName, wantPort string
-	} {
-
-		{"{ \"dbName\": \"ft\", \"server\": { \"port\" : \"1234\" } }", "ft", "1234"},
-
-	}
-
-	for _, c := range tests {
-		got, _ := readConfigFromReader(strings.NewReader(c.json))
-		var resDbName = got.DbName
-		var resServerPort = got.Server.Port
-		if resDbName != c.wantDbName ||
-			 resServerPort != c.wantPort {
-			t.Errorf("Input Json: %q, resultDbName: %q and resultPort: %q", c.json, resDbName, resServerPort)
-		}
-	}
-
-}
-
 func TestPrepareMgoUrls(t *testing.T) {
 	var tests = [] struct {
 		config Configuration
