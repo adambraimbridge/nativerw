7ac4fca6242a6d538022bb9347c495ddca8d297b
Hunor Kovács <hunor.kovacs@ft.com>
2015-04-28 15:53:54 +0300

    Adding supervisord config file with the scope of specifying the app's log file location.

--- puppet/ft-nativerw/manifests/supervisord.pp
+++ puppet/ft-nativerw/manifests/supervisord.pp
@@ -1,6 +1,7 @@
 class nativerw::supervisord {
 
   $supevisord_init_file = "/etc/init.d/supervisord"
+  $supevisord_conf_file = "/etc/supervisord.conf"
 
   satellitesubscribe {
     'gateway-epel':
@@ -26,6 +27,13 @@ class nativerw::supervisord {
       source    => "puppet:///modules/$module_name/supervisord.init",
       owner     => 'root',
       group     => 'root',
-      require   => [ Package['supervisor'] ]
+      require   => [ Package['supervisor'] ];
+
+    $supervisord_conf_file:
+      mode      => "0664",
+      source    => "puppet:///modules/$module_name/supervisord.conf",
+      owner     => 'root',
+      group     => 'root',
+      require   => [ Package['supervisor'] ];
   }
 }
