f88dac19d6975b9c88b819adcb8c977f48a52411
Hunor Kovács <hunor.kovacs@ft.com>
2015-04-29 12:32:37 +0300

    Adding nativerw and supervisord users. Assigning files and folders to them, trying to run supervisord not as root.

--- puppet/ft-nativerw/manifests/supervisord.pp
+++ puppet/ft-nativerw/manifests/supervisord.pp
@@ -1,44 +1,63 @@
 class nativerw::supervisord {
 
+  $supervisord_user = "supervisord"
   $supervisord_init_file = "/etc/init.d/supervisord"
   $supervisord_config_file = "/etc/supervisord.conf"
+  $supervisord_log_dir = "/var/log/supervisor/"
+  $binary_name = "nativerw"
 
-  satellitesubscribe {
-    'gateway-epel':
+  satellitesubscribe { 'gateway-epel':
       channel_name  => 'epel'
   }
 
-  package {
-    'python-pip':
+  package { 'python-pip':
       ensure    => 'installed',
       require   => Satellitesubscribe["gateway-epel"];
   }
 
-  package {
-    'supervisor':
+  package { 'supervisor':
       provider  => pip,
       ensure    => present,
       require   => [ Package['python-pip'] ]
   }
 
-  service { 'supervisord':
-    ensure      => running,
-    restart     => true
+  group { $supervisord_user:
+    name      => $supervisord_user,
+    ensure    => present,
+  }
+
+  user { $supervisord_user:
+    name      => $supervisord_user,
+    ensure    => present,
+    groups    => [ $supervisord_user, $binary_name ],
   }
 
   file {
     $supervisord_init_file:
       mode      => "0755",
       content    => template("$module_name/supervisord.init.erb"),
-      owner     => 'root',
-      group     => 'root',
-      require   => [ Package['supervisor'] ];
+      owner     => $supervisord_user,
+      group     => $supervisord_user,
+      require   => [ Package['supervisor'], User[$supervisord_user], Group[$supervisord_user] ];
 
     $supervisord_config_file:
       mode      => "0664",
       content    => template("$module_name/supervisord.conf.erb"),
-      owner     => 'root',
-      group     => 'root',
-      require   => [ Package['supervisor'] ];
+      owner     => $supervisord_user,
+      group     => $supervisord_user,
+      require   => [ Package['supervisor'], User[$supervisord_user], Group[$supervisord_user] ];
+
+    $supervisord_log_dir:
+      ensure    => directory,
+      owner     => $supervisord_user,
+      group     => $supervisord_user,
+      mode      => "0664",
+      require   => [ Package['supervisor'], User[$supervisord_user], Group[$supervisord_user] ];
+  }
+
+  service { 'supervisord':
+    ensure      => running,
+    restart     => true
+    require     => [ File[$supervisord_init_file], File[$supervisord_config_file], File[$supervisord_log_dir] ];
   }
 }
