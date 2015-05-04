2ed67719258597c011aa40afa4b32ee947d2f082
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-05-04 16:54:43 +0300

    readded 'expect' attribute of http_check

--- puppet/ft-nativerw/manifests/monitoring.pp
+++ puppet/ft-nativerw/manifests/monitoring.pp
@@ -11,6 +11,7 @@ class nativerw::monitoring {
     "${::certname}/1":
       url           => "http://${::hostname}/__health",
       port          => 8082,
+      expect        => "OK",
       size          => 1,
       action_url    => 'https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/nativerw-runbook',
       notes         => "Severity 1 \\n Service unavailable \\n Native Reader Writer healthchecks are failing. Please check http://${::hostname}:8082/__health \\n\\n"
