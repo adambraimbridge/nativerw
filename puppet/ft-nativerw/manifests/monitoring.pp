669db1c28a6c54648850221991e47241f93fc62a
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-05-04 14:46:13 +0300

    fixed nagios http check url

--- puppet/ft-nativerw/manifests/monitoring.pp
+++ puppet/ft-nativerw/manifests/monitoring.pp
@@ -9,10 +9,10 @@ class nativerw::monitoring {
 
   nagios::nrpe_checks::check_http {
     "${::certname}/1":
-      url           => "http://${::hostname}/healthcheck",
+      url           => "http://${::hostname}/__health",
       port          => 8082,
       size          => 1,
       action_url    => 'https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/nativerw-runbook',
-      notes         => "Severity 1 \\n Service unavailable \\n Native Reader Writer healthchecks are failing. Please check http://${::hostname}:8082/healthcheck \\n\\n"
+      notes         => "Severity 1 \\n Service unavailable \\n Native Reader Writer healthchecks are failing. Please check http://${::hostname}:8082/__health \\n\\n"
   }
 }
