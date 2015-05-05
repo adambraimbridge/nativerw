1439014d76976b87ee64aa31e5b64ed6b6c386a1
Hunor Kovács <hunor.kovacs@ft.com>
2015-05-05 11:52:31 +0300

    Removed check_http check.

--- puppet/ft-nativerw/manifests/monitoring.pp
+++ puppet/ft-nativerw/manifests/monitoring.pp
@@ -62,14 +62,4 @@ class nativerw::monitoring {
       port          => 8082,
       notes         => "check ${::certname} [$hostname] listening on HTTP port 8082 ";
   }
-
-  nagios::nrpe_checks::check_http {
-    "${::certname}/1":
-      url           => "http://${::hostname}/healthcheck",
-      port          => 8082,
-      expect        => "OK",
-      size          => 1,
-      action_url    => 'https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/nativerw-runbook',
-      notes         => "Severity 1 \\n Service unavailable \\n Native Reader Writer healthchecks are failing. Please check http://${::hostname}:8082/healthcheck \\n\\n"
-  }
 }
