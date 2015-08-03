6476c379e191c3f3d933e4026fb4cf5c3aa1be7a
larisa.lungu <larisa.lungu@ft.com>
2015-08-03 11:45:49 +0300

    Updated severity to 2.

--- healthcheck.go
+++ healthcheck.go
@@ -15,7 +15,7 @@ func (m *MgoApi) writeHealthCheck() fthealth.Check {
 		BusinessImpact:   "Publishing won't work. Writing content to native store is broken.",
 		Name:             "Write to mongoDB",
 		PanicGuide:       "https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/native-store-reader-writer-run-book",
-		Severity:         1,
+		Severity:         2,
 		TechnicalSummary: "Writing to mongoDB is broken. Check mongoDB is up, its disk space, ports, network.",
 		Checker:          m.checkWritable,
 	}
@@ -32,7 +32,7 @@ func (m *MgoApi) readHealthCheck() fthealth.Check {
 		BusinessImpact:   "Reading content from native store is broken.",
 		Name:             "Read from mongoDB",
 		PanicGuide:       "https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/native-store-reader-writer-run-book",
-		Severity:         1,
+		Severity:         2,
 		TechnicalSummary: "Reading from mongoDB is broken. Check mongoDB is up, its disk space, ports, network.",
 		Checker:          m.checkReadable,
 	}
