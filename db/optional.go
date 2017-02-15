c1491bd22e6d86ea12eb439c4691a51677af221b
Clark, Peter <peter.clark@ft.com>
2017-02-15 14:19:44 +0000

    Locking before update in optional

--- db/optional.go
+++ db/optional.go
@@ -1,18 +1,27 @@
 package db
 
+import "sync"
+
 // Optional initialises a variable given a suitable loader function
 type Optional struct {
 	val   interface{}
 	block func() error
+	mutex *sync.RWMutex
 }
 
 // Get returns the variable (initialised or not)
 func (o *Optional) Get() interface{} {
+	o.mutex.RLock()
+	defer o.mutex.RUnlock()
+
 	return o.val
 }
 
 // Nil checks whether the variable is initialised yet
 func (o *Optional) Nil() bool {
+	o.mutex.RLock()
+	defer o.mutex.RUnlock()
+
 	return o.val == nil
 }
 
@@ -20,6 +29,14 @@ func (o *Optional) Nil() bool {
 func (o *Optional) Block() (interface{}, error) {
 	err := o.block()
 	return o.Get(), err
+}
+
+// Update updates the optional var
+func (o *Optional) Update(val interface{}) {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
+	o.val = val
 }
 
 // NewOptional creates a new optional with the given loader func
@@ -32,11 +49,12 @@ func NewOptional(f func() (interface{}, error)) *Optional {
 			ch <- result
 			return result
 		},
+		mutex: &sync.RWMutex{},
 	}
 
 	go func() {
 		val, err := f()
-		optional.val = val
+		optional.Update(val)
 		ch <- err
 	}()
 
