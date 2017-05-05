df87666c18046181c5b34fe7c8554939f0e32414
Sandor Torok <sandor.torok@ft.com>
2017-05-05 15:56:45 +0300

    Removed brightcove references

--- config/config_test.go
+++ config/config_test.go
@@ -19,7 +19,7 @@ func TestConfigFromReader(t *testing.T) {
          },
          "dbName": "native-store",
          "collections": [
-            "brightcove",
+            "video",
             "methode",
             "wordpress",
             "v1-metadata"
@@ -30,7 +30,7 @@ func TestConfigFromReader(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, randomness, config.Mongos)
 	assert.Equal(t, "native-store", config.DbName)
-	assert.Equal(t, []string{"brightcove", "methode", "wordpress", "v1-metadata"}, config.Collections)
+	assert.Equal(t, []string{"video", "methode", "wordpress", "v1-metadata"}, config.Collections)
 	assert.Equal(t, "8080", config.Server.Port)
 }
 
@@ -55,7 +55,7 @@ func TestConfigFromFile(t *testing.T) {
          },
          "dbName": "native-store",
          "collections": [
-            "brightcove",
+            "video",
             "methode",
             "wordpress",
             "v1-metadata"
@@ -68,6 +68,6 @@ func TestConfigFromFile(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, randomness, config.Mongos)
 	assert.Equal(t, "native-store", config.DbName)
-	assert.Equal(t, []string{"brightcove", "methode", "wordpress", "v1-metadata"}, config.Collections)
+	assert.Equal(t, []string{"video", "methode", "wordpress", "v1-metadata"}, config.Collections)
 	assert.Equal(t, "8080", config.Server.Port)
 }
